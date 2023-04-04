0896a56f61870354a404d215188db054ed171d3a
pulse-8 <129873794+pulse-8@users.noreply.github.com>
2023-04-04 17:40:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+/**menu icon navbar */
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bxs-x-circle');
+    navbar.classList.toggle('active');
+};
+
+
+/**scroll section active link */
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+
+/**sticky navbar */
+    let header = document.querySelector('.header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+
+
+
+    /**remove menu when click(scroll) */
+    menuIcon.classList.remove('bxs-x-circle');
+    navbar.classList.remove('active');
+
+    };
+
+/**remove menu when click(scroll) */
+    menuIcon.classList.remove('bxs-x-circle');
+    navbar.classList.remove('active');
+
+
+ /**scroll reveal */
+    ScrollReveal({ 
+        reset: true,
+        distance: '80px',
+        duration: 2000,
+        delay: 200
+    });
+
+    ScrollReveal().reveal('.home-content, .heading', { 
+        origin: 'top' 
+    });
+
+     ScrollReveal().reveal('.home-img img, .service, .contact form, .container, .about-content', { 
+        origin: 'bottom' 
+    });
+     ScrollReveal().reveal('.home-content h1, .about-img img,.home-content ul', { 
+        origin: 'left' 
+    });
+     ScrollReveal().reveal('.home-content h3, .home-content p, .about-content h3,.giphy-embed', { 
+        origin: 'right' 
+    });
+
